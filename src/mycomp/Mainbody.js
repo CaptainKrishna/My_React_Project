67244611313b83f30c94095b01e8d46be4f68b90
Krishna Vishwakarma <krishvishwa888@gmail.com>
2022-06-24 23:18:29 +0530

    done

---
+++ src/mycomp/Mainbody.js
@@ -0,0 +1,22 @@
+import React from "react";
+export default function Mainbody() {
+    return (
+        <>
+            <div className="card col-6 mt-5 rounded-md m-auto" >
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a  className="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            <div className="card col-6 mt-5 m-auto">
+                <div className="card-body ">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a  className="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </>
+
+    )
+}
---
+++ src/mycomp/sidebar.js
@@ -0,0 +1,20 @@
+import React from "react";
+export default function(){
+    return(
+        <>
+        <div className="card col-2 md-d-none w-full bg-dark text-white moc">
+        <div className="m-2">
+            Main Heading
+            <div className="n">
+                <ul>
+                    <li>Home</li>
+                    <li>Home</li>
+                    <li>Home</li>
+                    <li>Home</li>
+                </ul>
+            </div>
+        </div>
+        </div>
+        </>
+    )
+}
